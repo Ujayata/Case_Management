9e611fa4bbaba5f3990f5dac9906029e60a18ebd
Malcom-gif <malcomchivheya@gmail.com>
2023-12-07 20:14:01 +0200

    Updated functions

--- src/index.ts
+++ src/index.ts
@@ -1,144 +1,148 @@
 import {
-    query,
-    update,
-    StableBTreeMap,
-    Vec,
-    Void,
-    ic,
-    Opt,
-    None,
-    text,
-    Canister,
-    
-  } from "azle";
+  query,
+  update,
+  StableBTreeMap,
+  Vec,
+  Opt,
+  None,
+  text,
+  Void,
+  Canister,
+  ic,
+} from "azle";
+import { v4 as uuidv4 } from "uuid";
+import {
+  Client,
+  ClientPayload,
+  Lawyer,
+  LawyerPayload,
+  Case,
+  CasePayload,
+  Witness,
+  WitnessPayload,
+  ClientId,
+  LawyerId,
+  CaseId,
+  WitnessId,
+} from "./types";
 
-  import { v4 as uuidv4 } from "uuid";
+let Clients = StableBTreeMap<ClientId, Client>(text, Client, 6);
+let Lawyers = StableBTreeMap<LawyerId, Lawyer>(text, Lawyer, 2);
+let Cases = StableBTreeMap<CaseId, Case>(text, Case, 3);
+let Witnesses = StableBTreeMap<WitnessId, Witness>(text, Witness, 4);
+let ClientCase = StableBTreeMap<ClientId, Vec<CaseId>>(text, Vec(text), 5);
 
-  import {
-    Client,
-    ClientPayload,
-    Lawyer,
-    LawyerPayload,
-    Case,
-    CasePayload,
-    Witness,
-    ClientId,
-    LawyerId,
-    CaseId,
-    WitnessId,
-    WitnessPayload,
-
-  } from "./types";
-  
-  let Clients = StableBTreeMap<ClientId, Client>(text, Client, 0);
-  let Lawyers = StableBTreeMap<LawyerId, Lawyer>(text, Lawyer, 0);
-  let Cases = StableBTreeMap<CaseId, Case>(text, Case, 0);
-  let Witnesses = StableBTreeMap<WitnessId, Witness>(text, Witness, 0);
-  let ClientCase = StableBTreeMap<ClientId, Vec<ClientId>>(text, Vec(text), 0);
-
-
-
-  export default Canister({
-
-    addClient: update([ClientPayload],Client, (payload) => {
-      let client : Client = {
-        id: uuidv4(),
-        Name: payload.Name,
-        Surname: payload.Surname,
-        Phonenumber: payload.Phonenumber,
-        Email: payload.Email,
-        Gender: payload.Gender,
-        Date_Of_Birth: ic.time(),
-        Occupation: payload.Occupation,
-        Marital_Status: payload.Marital_Status,
-        Nationality: payload.Nationality,
-        datJoined: ic.time(),
-
-      };
-      Clients.insert(client.id, client);
-      return client;
-    
-    }),
-
-    addLawyer: update([LawyerPayload], Lawyer,(payload) =>{
-      let lawyer: Lawyer = {
-        id: uuidv4(),
-        Title: payload.Title,
-        Name: payload.Name,
-        Surname: payload.Surname,
-        Phonenumber: payload.Phonenumber,
-        Email: payload.Email,
-        Case_Category: payload.Case_Category,
-        Case_Won: payload.Case_Won,
-        Case_Lost: payload.Case_Lost,
-        Experience: payload.Experience,
-        Nationality: payload.Nationality,
-        createdAt: ic.time(), 
-      };
-      Lawyers.insert(lawyer.id, lawyer);
-      return lawyer;
-
-    }),
-
-    addCase: update([CasePayload], Case, (payload) =>{
-      let caseData: Case = {
+export default Canister({
+  addClient: update([ClientPayload], Client, (payload) => {
+    let client: Client = {
       id: uuidv4(),
+      Name: payload.Name,
+      Surname: payload.Surname,
+      Phonenumber: payload.Phonenumber,
+      Email: payload.Email,
+      Gender: payload.Gender,
+      Date_Of_Birth: ic.time(),
+      Occupation: payload.Occupation,
+      Marital_Status: payload.Marital_Status,
+      Nationality: payload.Nationality,
+      datJoined: ic.time(),
+    };
+    Clients.insert(client.id, client);
+    return client;
+  }),
+
+  addLawyer: update([LawyerPayload], Lawyer, (payload) => {
+    let lawyer: Lawyer = {
+      id: uuidv4(),
+      Title: payload.Title,
+      Name: payload.Name,
+      Surname: payload.Surname,
+      Phonenumber: payload.Phonenumber,
+      Email: payload.Email,
+      Case_Category: payload.Case_Category,
+      Case_Won: 0,
+      Case_Lost: 0,
+      Experience: payload.Experience,
+      Nationality: payload.Nationality,
+      createdAt: ic.time(),
+    };
+    Lawyers.insert(lawyer.id, lawyer);
+    return lawyer;
+  }),
+
+  addCase: update([CasePayload], Case, (payload) => {
+    let caseId = uuidv4();
+    let caseData: Case = {
+      id: caseId,
       Case_name: payload.Case_name,
       CreatedAt: ic.time(),
       UpdatedAt: None,
       Description: payload.Description,
       Documents: payload.Documents,
       Timeline: payload.Timeline,
-      };
-      Cases.insert(caseData.id, caseData);
-      return caseData;
+      State: "Pending",
+      LawyerId: None,
+      ClientId: None,
+      WitnessIds: None,
+    };
+    Cases.insert(caseId, caseData);
+    return caseData;
+   }),   
+   
 
-    }),
-    
-    addWitness: update([WitnessPayload], Witness,(payload) =>{
-      let witness: Witness ={
-        id: uuidv4(), 
-        Fullname: payload.Fullname,
-        national_id: payload.national_id,
-        Testimony: payload.Testimony,
-      };
+  addWitness: update([WitnessPayload], Witness, (payload) => {
+    let witness: Witness = {
+      id: uuidv4(),
+      Fullname: payload.Fullname,
+      national_id: payload.national_id,
+      Testimony: payload.Testimony,
+    };
+    Witnesses.insert(witness.id, witness);
+    return witness;
+  }),
 
-      Witnesses.insert(witness.id, witness)
-      return witness;
-    }),
+  getClient: query([ClientId], Opt(Client), (id) => {
+    return Clients.get(id);
+  }),
 
-    //get client by id
-    getClient: query([text], Opt(Clients), (id) =>{
-      return Clients.get(id);
-    }),
+  getAllClients: query([], Vec(Client), () => {
+    return Clients.values();
+  }),
 
-    //get all clients
-    getAllClients: query([], Vec(Clients), () =>{
-      return Clients.values();
-    }),
+  getCase: query([CaseId], Opt(Case), (id) => {
+    return Cases.get(id);
+  }),
 
-    //get case by id
-    getCase: query([text], Opt(Cases), (id) =>{
-      return Cases.get(id);
-    }),
+  getAllCases: query([], Vec(Case), () => {
+    return Cases.values();
+  }),
 
-    //get all cases 
-    getCases: query([], Vec(Cases), () =>{
-      return Cases.values();
-    }),
+  assignLawyerToCase: update([CaseId, LawyerId], Void, (caseId, lawyerId) => {
+    let caseData = Cases.get(caseId);
+    if (caseData) {
+      caseData.LawyerId = lawyerId;
+      Cases.insert(caseId, caseData);
+    }
+    return;
+  }),
 
-    //delete Case
-    deleteCase: update([text], Void, (CaseId) => {
-        // delete case from cases
-        Cases.delete(CaseId);
-        // delete case from ClientCase
-        ClientCase.values().forEach((CaseIds: Vec<CaseId>) => {
-          let index = CaseIds.indexOf(CaseId);
-          if (index !== -1) {
-            CaseIds.splice(index, 1);
-          }
-        });
-        return;
-       }),
+  updateCaseState: update([CaseId, text], Void, (caseId, newState) => {
+    let caseData = Cases.get(caseId);
+    if (caseData) {
+      caseData.State = newState;
+      Cases.insert(caseId, caseData);
+    }
+    return;
+  }),
 
-  })
+  deleteCase: update([CaseId], Void, (caseId) => {
+    Cases.delete(caseId);
+    ClientCase.values().forEach((caseIds: Vec<CaseId>) => {
+      let index = caseIds.indexOf(caseId);
+      if (index !== -1) {
+        caseIds.splice(index, 1);
+      }
+    });
+    return;
+  }),
+});
--- src/types.ts
+++ src/types.ts
@@ -1,123 +1,105 @@
 import {
-    text,
-    Record,
-    nat64,
-    Opt,
-  } from "azle";
+  text,
+  nat64,
+  Vec,
+  Opt,
+  Record
+} from "azle";
 
-    export type ClientId = text;
-    export type LawyerId = text;
-    export type CaseId = text;
-    export type WitnessId = text;
+export const ClientId = text;
+export const LawyerId = text;
+export const CaseId = text;
+export const WitnessId = text;
 
- 
-  export const Client = Record({
-    id: text,
-    Name: text,
-    Surname: text,
-    Phonenumber: text,
-    Email: text,
-    Gender: text,
-    Date_Of_Birth: nat64,
-    Occupation: text,
-    Marital_Status: text,
-    Nationality: text,
-    datJoined: nat64,
-    
-    });
-    export type Client = typeof Client;
+export const Client = Record({
+  id: ClientId,
+  Name: text,
+  Surname: text,
+  Phonenumber: text,
+  Email: text,
+  Gender: text,
+  Date_Of_Birth: nat64,
+  Occupation: text,
+  Marital_Status: text,
+  Nationality: text,
+  datJoined: nat64,
+});
+export type Client = typeof Client;
 
-    export const ClientPayload = Record({
+export const ClientPayload = Record({
+  Name: text,
+  Surname: text,
+  Phonenumber: text,
+  Email: text,
+  Gender: text,
+  Occupation: text,
+  Marital_Status: text,
+  Nationality: text,
+});
+export type ClientPayload = typeof ClientPayload;
 
-    Name: text,
-    Surname: text,
-    Phonenumber: text,
-    Email: text,
-    Gender: text,
-    Date_Of_Birth: nat64,
-    Occupation: text,
-    Marital_Status: text,
-    Nationality: text,
+export const Lawyer = Record({
+  id: LawyerId,
+  Title: text,
+  Name: text,
+  Surname: text,
+  Phonenumber: text,
+  Email: text,
+  Case_Category: text,
+  Case_Won: nat64,
+  Case_Lost: nat64,
+  Experience: text,
+  Nationality: text,
+  createdAt: nat64,
+});
+export type Lawyer = typeof Lawyer;
 
-    });
+export const LawyerPayload = Record({
+  Title: text,
+  Name: text,
+  Surname: text,
+  Phonenumber: text,
+  Email: text,
+  Case_Category: text,
+  Experience: text,
+  Nationality: text,
+});
+export type LawyerPayload = typeof LawyerPayload;
 
-    export type ClientPayload = typeof ClientPayload;
-    
-    export const Lawyer = Record({
-    id: text,
-    Title: text,
-    Name: text,
-    Surname: text,
-    Phonenumber: text,
-    Email: text,
-    Case_Category: text,
-    Case_Won: text,
-    Case_Lost: text,
-    Experience: text,
-    Nationality: text,
-    createdAt: nat64, 
+export const Case = Record({
+  id: CaseId,
+  Case_name: text,
+  CreatedAt: nat64,
+  UpdatedAt: Opt(nat64),
+  Description: text,
+  Documents: Vec(text),
+  Timeline: text,
+  State: text,
+  LawyerId: Opt(LawyerId),
+  ClientId: Opt(ClientId),
+  WitnessIds: Opt(Vec(WitnessId)),
+});
+export type Case = typeof Case;
 
-    });
-    
-    export type Lawyer = typeof Lawyer;
+export const CasePayload = Record({
+  Case_name: text,
+  Description: text,
+  Documents: Vec(text),
+  Timeline: text,
+});
+export type CasePayload = typeof CasePayload;
 
-    export const LawyerPayload = Record({
-        Title: text,
-        Name: text,
-        Surname: text,
-        Phonenumber: text,
-        Email: text,
-        Case_Category: text,
-        Case_Won: text,
-        Case_Lost: text,
-        Experience: text,
-        Nationality: text,
-    
-        });
-        
-    export type LawyerPayload = typeof LawyerPayload;
-    
-    export const Case = Record({
-    id: text,
-    Case_name: text,
-    CreatedAt: nat64,
-    UpdatedAt: Opt(nat64),
-    Description: text,
-    Documents: text,
-    Timeline: text,
-    
-    });
-    
-    export type Case = typeof Case;
+export const Witness = Record({
+  id: WitnessId,
+  Fullname: text,
+  national_id: text,
+  Testimony: text,
+});
+export type Witness = typeof Witness;
 
-    export const CasePayload = Record({
-        Case_name: text,
-        UpdatedAt: Opt(nat64),
-        Description: text,
-        Documents: text,
-        Timeline: text,
-
-        });
-    
-    export type CasePayload = typeof CasePayload;
-
-    export const Witness = Record ({
-    id: text, 
-    Fullname: text,
-    national_id: text,
-    Testimony: text,
-    
-    });
-    export type Witness = typeof Witness;
-
-    export const WitnessPayload = Record ({
-        id: text, 
-        Fullname: text,
-        national_id: text,
-        Testimony: text,
-        
-        });
-    export type WitnessPayload = typeof WitnessPayload;
-    
-    
-    
+export const WitnessPayload = Record({
+  Fullname: text,
+  national_id: text,
+  Testimony: text,
+});
+export type WitnessPayload = typeof WitnessPayload;
